0f443c478265af562387462ea4b0ef3404dd2add
Armenion <carlohilo0000@gmail.com>
2026-02-04 00:24:44 +0000

    update

---
+++ Website/receiveBox.php
@@ -0,0 +1,14 @@
+<?php
+
+$conn = pg_connect("host=dpg-d60lqccr85hc7397h08g-a 
+dbname=testdb_2bq6 
+user=armenion 
+password=d3gK0fljNZaEyKkk7PerytN32qSkIn6A");
+
+if( !$conn){
+    die("Cannot Link Start.");
+}
+
+$input = $_POST['input'];
+
+?>
